03aded91d3cb4a579629f9dd49aa065c2fde40a3
Art <voziart@gmail.com>
2022-02-04 17:22:37 +0500

    init

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div className='footer' style={{ textAlign: 'center' }}>
+			<span>Simple OAuth with Node.js</span>
+		</div>
+	);
+};
+
+export default Footer;
